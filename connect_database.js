a6ea9c2eb16c49a178d120b9ed6d0d49113ad394
projetoscti28 <suporteatmovision@outlook.com>
2024-09-23 10:24:12 -0300

    Add files via upload

---
+++ connect_database.js
@@ -0,0 +1,56 @@
+const mqtt = require('mqtt');
+const { Pool, Client } = require('pg');
+
+//Conexao do Banco
+const client_database = new Client(
+    {
+        user: 'projetoscti27',
+        password: 'eq53B965',
+        host: 'pgsql.projetoscti.com.br',
+        database: 'projetoscti27',
+    })
+
+client_database.connect();
+
+// Conexao MQTT
+const port = "1883";
+const host = `mqtt://192.168.1.112:${port}`;
+
+var client = mqtt.connect(host);
+let topic = '#';
+
+client.on('connect', function success()
+{
+  console.log("Connecting MQTT");
+  client.subscribe(topic, function mqtt_subscribe()
+  {
+      console.log("Subscribe to" + topic);
+  });
+});
+
+client.on('message', mqtt_message);
+
+function mqtt_message(topic, message)
+{
+    var J = JSON.parse(message);
+    var T = J.Temperature;
+    var H = J.Humidity;
+    var D = J.Date;
+    var I = J.Id;
+    console.log('topic'+ topic + ' Message' + message);
+
+    // Insercao
+    const q="INSERT INTO dht11_data(id, temperature, humidity, date) VALUES($1,$2,$3,$4) RETURNING*";
+    const v =[I, T, H, D]; 
+    
+    client_database.query(q,v, (err, res) => {
+      if (err) 
+      {
+        console.log(err.stack)
+      } 
+      else 
+      {
+        console.log(res.rows[0])
+      }
+    });
+}
